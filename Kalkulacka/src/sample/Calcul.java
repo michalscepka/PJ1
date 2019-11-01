28acfdefaf1fe943a92496a5b189944024b3a0bc
Strojj <scepkamichal1@gmail.com>
2019-11-01 01:32:57 +0100

    1.1

--- Kalkulacka/src/sample/Calcul.java
+++ Kalkulacka/src/sample/Calcul.java
@@ -1,8 +1,6 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -33,12 +31,7 @@ public class Calcul extends Application {
         for(int i = 0; i < 10; i++) {
             Button btn = new Button();
             btn.setText(Integer.toString(i));
-            btn.setOnAction(new EventHandler<ActionEvent>() {
-                @Override
-                public void handle(ActionEvent event) {
-                    numbersInput(btn.getText());
-                }
-            });
+            btn.setOnAction(event -> numbersInput(btn.getText()));
             //g.getChildren().add(btn);
             g.add(btn, i, 1);
         }
@@ -46,43 +39,23 @@ public class Calcul extends Application {
         for(int i = 0; i < znaky_pocet; i++) {
             Button btn = new Button();
             btn.setText(znaky[i]);
-            btn.setOnAction(new EventHandler<ActionEvent>() {
-                @Override
-                public void handle(ActionEvent event) {
-                    operandsInput(btn.getText());
-                }
-            });
+            btn.setOnAction(event -> operandsInput(btn.getText()));
             g.add(btn, i, 2);
         }
 
         Button btn_eqv = new Button();
         btn_eqv.setText("=");
-        btn_eqv.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                equalsInput(btn_eqv.getText());
-            }
-        });
+        btn_eqv.setOnAction(event -> equalsInput());
         g.add(btn_eqv, 4, 2);
 
         Button btn_C = new Button();
         btn_C.setText("C");
-        btn_C.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                cInput(btn_C.getText());
-            }
-        });
+        btn_C.setOnAction(event -> cInput());
         g.add(btn_C, 5, 2);
 
         Button btn_dot = new Button();
         btn_dot.setText(".");
-        btn_dot.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                numbersInput(btn_dot.getText());
-            }
-        });
+        btn_dot.setOnAction(event -> numbersInput(btn_dot.getText()));
         g.add(btn_dot, 6, 2);
 
         Scene scene = new Scene(g, 600, 250);
@@ -100,12 +73,12 @@ public class Calcul extends Application {
         System.out.println("Key" + x + " was pressed");
 
         if(can_clear_label) {
-            label1.setText(x.toString());
+            label1.setText(x);
             can_clear_label = false;
         }
         else {
             String label_text = label1.getText();
-            label1.setText(label_text + x.toString());
+            label1.setText(label_text + x);
         }
     }
 
@@ -113,21 +86,21 @@ public class Calcul extends Application {
         System.out.println("Key" + x + " was pressed");
 
         String label_text = label1.getText();
-        label1.setText(label_text + x.toString());
+        label1.setText(label_text + x);
         operands.add(x);
         can_clear_label = false;
     }
 
-    private void equalsInput(String x) {
-        String[] cislaS = label1.getText().split("\\+|-|\\*|/");
+    private void equalsInput() {
+        String[] cislaS = label1.getText().split("[+\\-*/]");
         double[] cisla = new double[cislaS.length];
 
         for(int i = 0; i < cisla.length; i++) {
             System.out.println(cislaS[i]);
             cisla[i] = Double.parseDouble(cislaS[i]);
         }
-        for(int i = 0; i < operands.size(); i++) {
-            System.out.println(operands.get(i));
+        for (String operand : operands) {
+            System.out.println(operand);
         }
 
         double vysledek = cisla[0];
@@ -157,7 +130,7 @@ public class Calcul extends Application {
         can_clear_label = true;
     }
 
-    private void cInput(String x) {
+    private void cInput() {
         clear();
     }
 
