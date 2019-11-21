54166b18dfde412fac4df4f9649b11b681ad32d0
ajmitch11 <scepkamichal1@gmail.com>
2019-11-22 00:58:02 +0100

    Revert "mines colors added"
    This reverts commit b5b54864

--- MinesweeperFX/src/minesweeperfx/gui/BoardCanvas.java
+++ MinesweeperFX/src/minesweeperfx/gui/BoardCanvas.java
@@ -16,7 +16,6 @@ public class BoardCanvas extends Canvas {
     private Board board;
     private final double widthTick;
     private final double heightTick;
-    private boolean isFirstMove;
 
     public BoardCanvas(Board board, double width, double height) {
         super(width, height);
@@ -32,14 +31,10 @@ public class BoardCanvas extends Canvas {
                 placeFlag(x, y);
             }
         });
-        isFirstMove = true;
     }
 
     public void printBoard() {
         GraphicsContext gc = getGraphicsContext2D();
-        gc.setFill(Color.CYAN);
-        gc.fillRect(0, 0, 500, 500);
-        gc.setFill(Color.BLACK);
         for(int i = 0; i < board.getSize() + 1; i++) {
             gc.strokeLine(i * widthTick, 0, i * widthTick, getHeight());
         }
@@ -50,34 +45,19 @@ public class BoardCanvas extends Canvas {
 
     private void makeMove(int x, int y) {
         LinkedList<Field> list = new LinkedList<>();
-        if(isFirstMove) {
-            board.initializeBoard(x, y);
-            isFirstMove = false;
-        }
         board.makeMove(x, y, list);
         GraphicsContext gc = getGraphicsContext2D();
         for(Field f : list) {
             if(f.getMark() == Mark.MINE) {
-                System.out.println("Game over");
-                gc.setFill(Color.ORANGERED);
-                gc.fillRect(x * widthTick + 1, y * heightTick + 1, widthTick - 2, heightTick - 2);
-                gc.setFill(Color.BLACK);
-                gc.setFont(new Font("Times", 20));
-                gc.fillText("\uD83D\uDCA3", x * widthTick + (widthTick - 20) / 1.9, (y + 1) * heightTick - (heightTick - 20) / 1.8);
+                gc.setFill(Color.RED);
+                gc.fillOval(f.getX() * widthTick + 2, f.getY() * heightTick + 2, widthTick - 4, heightTick - 4);
                 gc.setFill(Color.BLACK);
             } else if(f.getMark() == Mark.NUMBER) {
-                gc.setFill(Color.WHITE);
-                gc.fillRect(f.getX() * widthTick + 1, f.getY() * heightTick + 1, widthTick - 2, heightTick - 2);
-                gc.setFill(Color.BLACK);
                 gc.setFont(new Font("Times", 20));
-                gc.fillText(
-                        Integer.toString(board.getNumber(f.getX(), f.getY())),
-                        f.getX() * widthTick + (widthTick - 20) / 1.5,
-                        (f.getY() + 1) * heightTick - (heightTick - 20) / 1.8);
-            } else if(f.getMark() == Mark.EMPTY) {
-                gc.setFill(Color.WHITE);
-                gc.fillRect(f.getX() * widthTick + 1, f.getY() * heightTick + 1, widthTick - 2, heightTick - 2);
-                gc.setFill(Color.BLACK);
+                gc.fillText(Integer.toString(board.getNumber(
+                        f.getX(), f.getY())),
+                        f.getX() * widthTick + (widthTick - 20) / 2,
+                        (f.getY() + 1) * heightTick - (heightTick - 20) / 2);
             }
         }
     }
@@ -85,15 +65,10 @@ public class BoardCanvas extends Canvas {
     private void placeFlag(int x, int y) {
         GraphicsContext gc = getGraphicsContext2D();
         if(board.placeFlag(x, y)) {
-            gc.setFill(Color.YELLOW);
-            gc.fillRect(x * widthTick + 1, y * heightTick + 1, widthTick - 2, heightTick - 2);
-            gc.setFill(Color.rgb(128, 0, 32));
-            gc.setFont(new Font("Times", 20));
-            gc.fillText("\u2691", x * widthTick + (widthTick - 20) / 1.5, (y + 1) * heightTick - (heightTick - 20) / 1.8);
-            gc.setFill(Color.BLACK);
+            gc.fillOval(x * widthTick + 2, y * heightTick + 2, widthTick - 4, heightTick - 4);
         } else {
-            gc.setFill(Color.CYAN);
-            gc.fillRect(x * widthTick + 1, y * heightTick + 1, widthTick - 2, heightTick - 2);
+            gc.setFill(Color.WHITE);
+            gc.fillOval(x * widthTick + 1, y * heightTick + 1, widthTick - 2, heightTick - 2);
             gc.setFill(Color.BLACK);
         }
     }
