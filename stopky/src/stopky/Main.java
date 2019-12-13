54f05e233a8448caf674f85b708a632924e06c0e
michalscepka <scepkamichal1@gmail.com>
2019-12-13 16:43:16 +0100

    stopky final

--- stopky/src/stopky/Main.java
+++ stopky/src/stopky/Main.java
@@ -5,70 +5,84 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
+
+import java.time.LocalTime;
+import java.util.ArrayList;
 
 public class Main extends Application {
 
     private Vlakno t1;
-    private long startTime;
-    private Label label1;
-    private boolean started = false;
+    private Label currentTimeLabel;
+    private Label timesListLabel;
+    private ArrayList<String> times;
+    private Button btn_start;
+    private Button btn_snap;
+    private Button btn_pause;
+    private Button btn_stop;
 
     @Override
     public void start(Stage primaryStage) {
-        GridPane root = new GridPane();
-        primaryStage.setTitle("Hello World");
+
+        Pane root = new Pane();
+        primaryStage.setTitle("Stopky");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
 
-        label1 = new Label("Time:");
-        root.add(label1, 0, 1);
+        currentTimeLabel = new Label("000");
+        currentTimeLabel.setTranslateX(10);
+        currentTimeLabel.setTranslateY(10);
+        currentTimeLabel.setFont(new Font("Consolas", 24));
+        root.getChildren().add(currentTimeLabel);
 
-        Button btn_start = new Button();
+        timesListLabel = new Label();
+        timesListLabel.setTranslateX(15);
+        timesListLabel.setTranslateY(80);
+        timesListLabel.setFont(new Font("Consolas", 14));
+        root.getChildren().add(timesListLabel);
+
+        btn_start = new Button();
         btn_start.setText("Start");
+        btn_start.setTranslateX(10);
+        btn_start.setTranslateY(45);
         btn_start.setOnAction(event -> startInput());
-        root.add(btn_start, 0, 0);
+        root.getChildren().add(btn_start);
 
-        Button btn_snap = new Button();
+        btn_snap = new Button();
         btn_snap.setText("Snap");
+        btn_snap.setTranslateX(60);
+        btn_snap.setTranslateY(45);
+        btn_snap.setDisable(true);
         btn_snap.setOnAction(event -> snapInput());
-        root.add(btn_snap, 1, 0);
+        root.getChildren().add(btn_snap);
 
-        Button btn_pause = new Button();
+        btn_pause = new Button();
         btn_pause.setText("Pause");
-        btn_pause.setOnAction(event -> {
-            try {
-                pauseInput();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
-        root.add(btn_pause, 2, 0);
+        btn_pause.setTranslateX(110);
+        btn_pause.setTranslateY(45);
+        btn_pause.setDisable(true);
+        btn_pause.setOnAction(event -> pauseInput());
+        root.getChildren().add(btn_pause);
 
-        Button btn_stop = new Button();
+        btn_stop = new Button();
         btn_stop.setText("Stop");
+        btn_stop.setTranslateX(175);
+        btn_stop.setTranslateY(45);
+        btn_stop.setDisable(true);
         btn_stop.setOnAction(event -> stopInput());
-        root.add(btn_stop, 3, 0);
+        root.getChildren().add(btn_stop);
 
         new AnimationTimer() {
             @Override
             public void handle(long currentNanoTime) {
-                if(started)
-                    label1.setText(String.valueOf("Time: " + t1.getCurrentTime()));
+                try {
+                    currentTimeLabel.setText(t1.toString());
+                } catch (Exception ignored) {}
+
             }
         }.start();
-
-        /*Thread t1 = new Vlakno("Vlakno 1");
-        Thread t2 = new Vlakno("Vlakno 2");
-        Thread t3 = new Vlakno("Vlakno 3");
-
-        t1.start();
-        t2.start();
-        t3.start();
-
-        Thread.sleep(3000);
-        t1.interrupt();*/
     }
 
     public static void main(String[] args) {
@@ -76,26 +90,55 @@ public class Main extends Application {
     }
 
     private void startInput() {
-        //System.out.println("Start");
-        startTime = System.currentTimeMillis();
-        t1 = new Vlakno(startTime);
+        t1 = new Vlakno(LocalTime.now());
         t1.start();
-        started = true;
+
+        times = new ArrayList<>();
+        timesListLabel.setText("");
+
+        btn_pause.setText("Pause");
+
+        btn_start.setDisable(true);
+        btn_snap.setDisable(false);
+        btn_pause.setDisable(false);
+        btn_stop.setDisable(false);
     }
 
     private void snapInput() {
-        //System.out.println("Snap");
-        //System.out.println(System.nanoTime() / 1000000000.0);
-        //System.out.println(System.currentTimeMillis() - startTime);
-        //label1.setText(String.valueOf(System.currentTimeMillis() - startTime));
+        times.add(t1.toString());
+        StringBuilder sb = new StringBuilder();
+        for (int i = times.size() - 1; i >= 0; i--) {
+            sb.append(i).append(":\t").append(times.get(i)).append('\n');
+        }
+        timesListLabel.setText(sb.toString());
     }
 
-    private void pauseInput() throws InterruptedException {
-        System.out.println("Pause");
+    private void pauseInput() {
+        t1.setWait();
+        if(!t1.isWait()) {
+            btn_pause.setText("Pause");
+            t1.setResumedTime(LocalTime.now());
+            t1.calculatePauseDuration();
+
+            btn_start.setDisable(true);
+            btn_snap.setDisable(false);
+        } else {
+            btn_pause.setText("Resume");
+            snapInput();
+            t1.setPausedTime(LocalTime.now());
+
+            btn_start.setDisable(true);
+            btn_snap.setDisable(true);
+        }
+        btn_pause.setDisable(false);
+        btn_stop.setDisable(false);
     }
 
     private void stopInput() {
-        System.out.println("Stop");
         t1.interrupt();
+        btn_start.setDisable(false);
+        btn_snap.setDisable(true);
+        btn_pause.setDisable(true);
+        btn_stop.setDisable(true);
     }
 }
--- stopky/src/stopky/Vlakno.java
+++ stopky/src/stopky/Vlakno.java
@@ -1,30 +1,77 @@
 package stopky;
 
+import java.time.Duration;
+import java.time.LocalTime;
+
 public class Vlakno extends Thread {
 
-    private long startTime;
-    private long currentTime;
+    private LocalTime startTime;
+    private Duration currentTime;
+    private boolean wait = false;
+    private LocalTime pausedTime;
+    private LocalTime resumedTime;
 
-    public Vlakno(long startTime) {
+    public Vlakno(LocalTime startTime) {
         this.startTime = startTime;
     }
 
     @Override
     public void run() {
-        while(true) {
-            //System.out.println(System.currentTimeMillis() - startTime);
-            currentTime = System.currentTimeMillis() - startTime;
-            //label.setText(String.valueOf(currentTime));
-            try {
-                Thread.sleep(0);
-            } catch (InterruptedException e) {
-                System.err.println("Preruseno");
-                return;
+        while(!isInterrupted()) {
+            if(wait) {
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException ignore) {}
+            } else {
+                currentTime = Duration.between(startTime, LocalTime.now());
             }
         }
+        System.err.println("Preruseno");
     }
 
-    public long getCurrentTime() {
+    public Duration getCurrentTime() {
         return currentTime;
+    }
+
+    public boolean isWait() {
+        return wait;
+    }
+
+    public void setWait() {
+        wait = !wait;
+    }
+
+    public void setPausedTime(LocalTime pausedTime) {
+        this.pausedTime = pausedTime;
+    }
+
+    public void setResumedTime(LocalTime resumedTime) {
+        this.resumedTime = resumedTime;
+    }
+
+    public void calculatePauseDuration() {
+        Duration pauseDuration = Duration.between(pausedTime, resumedTime);
+        //System.out.println("pause duration: " + pauseDuration);
+        startTime = startTime.plus(Duration.ofHours(pauseDuration.toHoursPart()));
+        startTime = startTime.plus(Duration.ofMinutes(pauseDuration.toMinutesPart()));
+        startTime = startTime.plus(Duration.ofSeconds(pauseDuration.toSecondsPart()));
+        startTime = startTime.plus(Duration.ofMillis(pauseDuration.toMillisPart()));
+        //System.out.println("new start time: " + startTime);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        if(getCurrentTime().toHoursPart() != 0)
+            sb.append(getCurrentTime().toHoursPart()).append(":");
+        if(getCurrentTime().toMinutesPart() != 0)
+            sb.append(getCurrentTime().toMinutesPart()).append(":");
+        if(getCurrentTime().toSecondsPart() != 0)
+            sb.append(getCurrentTime().toSecondsPart()).append(":");
+        if(getCurrentTime().toMillisPart() != 0)
+            sb.append(getCurrentTime().toMillisPart());
+
+        return sb.toString();
     }
 }
